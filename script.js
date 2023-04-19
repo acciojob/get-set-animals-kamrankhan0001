a4ff719ef261636ce1505f8789054ea249e59f4f
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-19 10:46:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this._species=species;
-	}
-	get species(){
-		return this._species;
-	}
-	makeSound(){
-		//console.log(`the  ${this._species} make a sound.`);
-		//console.log("The " + ${this._species} + " makes a sound.");
-		console.log("the animal makes a sound");
-	}
+  constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("The " + this.species + " makes a sound.");
+  }
 }
+
 class Cat extends Animal {
-	purr(){
-	console.log("purr");	
-	}
+  purr() {
+    console.log("purr");
+  }
 }
 
 class Dog extends Animal {
-	bark(){
-		console.log("woof");
-	}
+  bark() {
+    console.log("woof");
+  }
 }
+
 
 
 // const animal = new Animal("unknown");
