82ef8e1342a585efcf95f50b7fa02aab55e50523
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-19 10:35:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,19 +8,19 @@ class Animal {
 	}
 	makeSound(){
 		//console.log(`the  ${this._species} make a sound.`);
-		console.log("The " + this._species + " makes a sound.");
+		console.log("The " + ${this._species} + " makes a sound.");
 	}
 }
 
 class Dog extends Animal {
 	bark(){
-		console.log("The Golden Retriever makes a sound");
+		console.log('woof');
 	}
 }
 
 class Cat extends Animal {
 	purr(){
-	console.log("The Siamese makes a sound");	
+	console.log('purr');	
 	}
 }
 // const animal = new Animal("unknown");
